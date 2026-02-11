27eb12391759920b1d9f03ad584f0e139ed440ee
Cole Moore <Cartisim@icloud.com>
2026-02-11 17:37:18 +0800

    Updated packages

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/needletails/needletail-crypto.git", from: "1.2.1"),
-        .package(url: "https://github.com/needletails/needletail-logger.git", from: "3.1.1"),
+        .package(url: "https://github.com/needletails/needletail-logger.git", from: "3.1.4"),
         .package(url: "https://github.com/needletails/binary-codable.git", from: "1.0.3")
     ],
     targets: [
